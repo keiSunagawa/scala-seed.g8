9f8188a55274ca1f032c3fbb2fac73e0a0a0d4b7
keiSunagawa <keisunagawa.git@icloud.com>
2020-01-29 23:04:52 +0900

    fix org

--- src/main/g8/project/dependencies.scala
+++ src/main/g8/project/dependencies.scala
@@ -3,10 +3,10 @@ import sbt._
 object Versions {
   lazy val scalaTest = "3.1.0"
   lazy val cats = "2.1.0"
-  lazy val kerfumeUtil = "v0.1.0"
+  lazy val kerfumeUtil = "0.1.0"
 }
 object Dependencies {
   lazy val testDep = Seq("org.scalatest" %% "scalatest" % Versions.scalaTest)
   lazy val cats = Seq("org.typelevel" %% "cats-core" % Versions.cats)
-  lazy val kerfumeUtil = Seq("me.kerfume" %% "kerfume-scala-util-core" % Versions.kerfumeUtil)
+  lazy val kerfumeUtil = Seq("com.github.keiSunagawa" % "kerfume-scala-util" % Versions.kerfumeUtil)
 }
